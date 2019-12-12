55aea32d759812f41a2c878c4e12b4a4707dbcca
Feli <srgatoiscool@gmail.com>
2019-12-12 12:02:50 -0500

    added tks

---
+++ 0x02-python-import_modules/test_files/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+
+    a = 5
+    b = 10
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
