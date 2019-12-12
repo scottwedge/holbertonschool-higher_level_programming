bc592c75ebc9aa6d032cd6bde82b1e99ac1373f7
Feli <srgatoiscool@gmail.com>
2019-12-12 13:06:14 -0500

    added fix files

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -2,43 +2,50 @@
 def add(a, b):
     """My addition function
 
-        Args:
-a: first integer
-b: second integer
+    Args:
+        a: first integer
+        b: second integer
 
-Returns:
-The return value. a + b
+    Returns:
+        The return value. a + b
     """
     return (a + b)
 
+
 def sub(a, b):
+    """My subtraction function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a - b
+    """
     return (a - b)
 
 
-def mul(a, b):  
-    return(a * b)
+def mul(a, b):
+    """My multiplication function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
 
 
 def div(a, b):
-    """My
-        division
-        function
+    """My division function
 
-        Args:
-a:
-first
-integer
-b:
-second
-integer
+    Args:
+        a: first integer
+        b: second integer
 
-Returns:
-The
-return
-value.
-a
-/
-b
-"""
+    Returns:
+        The return value. a / b
+    """
     return int(a / b)
-
