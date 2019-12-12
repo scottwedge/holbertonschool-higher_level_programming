15f04d1fe77765b52cee51348a6e98a030f53d5c
Feli <srgatoiscool@gmail.com>
2019-12-12 13:52:09 -0500

    fix pep8

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     import sys
     count = len(sys.argv)
     if count == 1:
-        print("{:d} arguments.".format(count- 1))
+        print("{:d} arguments.".format(count - 1))
     else:
         if count== 2:
-            print("{:d} argument:".format(count- 1))
+            print("{:d} argument:".format(count - 1))
         else:
-            print("{:d} arguments:".format(count- 1))
+            print("{:d} arguments:".format(count - 1))
         for av in range(1, count):
             print("{:d}: {}".format(av, (sys.argv[av])))
