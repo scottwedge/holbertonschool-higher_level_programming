ee053e561d1ad9602153de2bbab6ec90a6c94b9f
Feli <srgatoiscool@gmail.com>
2019-12-17 12:46:30 -0500

    added

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    lenList = len(my_list)
+    if lenList == 0:
+        return None
+    for i in my_list:
+        num = my_list[i]
+        if num < my_list[i]:
+            num = my_list[i]
+        return num
