8714ac8b8e3fabb3ade591fe61e63ec77067ed29
Feli <srgatoiscool@gmail.com>
2020-01-08 11:46:28 -0500

    added 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return True
+    else:
+        return False
