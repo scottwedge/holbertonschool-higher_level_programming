ef65115966f1e12c9670235fc18b42bf12c7c320
Feli <srgatoiscool@gmail.com>
2020-01-09 16:56:33 -0500

    luls

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,11 +46,12 @@ class Square:
         """
         if type(value) != tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if any(type(i) != int for i in value):
+        elif any(type(i) != int for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if any(i < 0 for i in value):
+        elif any(i < 0 for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        else:
+            self.__position = value
 
     def area(self):
         """ return the area
@@ -61,26 +62,12 @@ class Square:
 
     def my_print(self):
         """that prints in stdout the square with the character #"""
-        if self.__size > 0:
-            for i in range(self.__position[1]):
-                print()
-            for j in range(self.__size):
-                for k in range(self.__position[0]):
-                    print(" ", end="")
-                for l in range(self.__size):
-                    print("#", end="")
-                print()
-        else:
-            print()
-"""         if self.__size == 0:
+        if self.__size == 0:
             print("")
             return
 
-
-       [print("") for i in range(self.__position[1])]
+        [print("") for i in range(self.__position[1])]
         for j in range(self.__size):
             [print(" ", end="") for k in range(self.__position[0])]
             [print("#", end="") for l in range(self.__size)]
             print("")
-"""
-
