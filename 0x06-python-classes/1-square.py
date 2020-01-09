37f87fa521bf7cde31ff65b331e26919e5273372
Feli <srgatoiscool@gmail.com>
2020-01-09 10:50:34 -0500

    add #1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 1-square.py)"""
+
+
+class Square:
+    """Write a class Square that defines a square by: (based on 1-square.py)"""
+    def __init__(self, size=0):
+        """Inizialitation
+
+        Args:
+            size (int): Private instance attribute: size.
+        """
+        self.__size = size
