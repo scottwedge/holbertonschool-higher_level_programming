54c6dc812b949739375b7e56ba0da99898014b2f
Feli <srgatoiscool@gmail.com>
2020-01-11 11:12:14 -0500

    add #0 and README

---
+++ 0x07-python-test_driven_development/0-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+add_integer = __import__('0-add_integer').add_integer
+print(add_integer(4.12, "School"))
+"""try:
+    print(add_integer(5, 5, 5))
+except Exception as e:
+    print(e)
+try:
+    print(add_integer(None))
+except Exception as e:
+    print(e)"""
