5d19b0952b3458cf10d42d51e544b78c4dd6f35d
Feli <srgatoiscool@gmail.com>
2020-01-21 10:02:39 -0500

    add #10

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,7 @@
+"""Write a function that returns the dictionary description
+    with simple data structure (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object:"""
+
+
+def class_to_json(obj):
+    return obj.__dict__
