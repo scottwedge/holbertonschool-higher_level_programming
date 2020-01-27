9936a92d3d52b32e69ec8a9d20ad7b1c887e6ede
Feli <srgatoiscool@gmail.com>
2020-01-27 13:23:10 -0500

    add

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,6 +4,8 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """ rectangle class"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """"inicialization constructor"""
         """Call the super class with id"""
